15f8d8abc3cba40cd5c3a5be7232456d5a8afce0
Colin-TUE <c.lambrechts@student.tue.nl>
2014-05-20 12:29:55 +0200

    Added the table generator
    The table generator generates the table that need to e inserted in the
    DB (one time only probaly). The format of the tables Jules and I decided
    on is "edge#x#" where the #'s are the number of the nodes. And they are
    in sorted order (the smallest first).

---
+++ Query Generator/src/generators/TableGenerator.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generators;
+
+/**
+ * Class for generating the tables needed in the db
+ *
+ * @author Colin Lambrechts
+ */
+public class TableGenerator {
+
+    /**
+     * The separator between tables
+     */
+    private static final String SEPARATOR = "x";
+
+    /**
+     * The line separator on this system
+     */
+    private static final String NEWLINE = System.getProperty("line.separator");
+
+    /**
+     * All possible combinations of colours that are possible
+     */
+    private static final Pair[] COLOURCOMBINATIONS = new Pair[]{
+        new Pair(1, 2), new Pair(1, 3),
+        new Pair(2, 1), new Pair(2, 3),
+        new Pair(3, 1), new Pair(3, 2)};
+
+    /**
+     * Generates a SQL statement in which all possible (undirected) edges are
+     * represented as tables. For each edge there is a table with all
+     * possibilities in which the nodes can be coloured. The name of the table
+     * is "edge#x#" in which the first # is the first node number and the second
+     * # is the second number. The #'s are sorted, so the smallest number will
+     * be the first The name of the two columns are the numbers of the nodes
+     *
+     * @param nrVertices the number of vertices
+     * @return the SQL statement needed to insert the tables
+     */
+    public static String GenerateTable(int nrVertices) {
+        String statement = "";
+
+        //Generate the complete graph so do a double for loop
+        for (int i = 1; i <= nrVertices; i++) {
+            for (int j = i + 1; j <= nrVertices; j++) {
+                // First create table
+                String ct = "CREATE TABLE " // create statement/command
+                        + "edge" + i + TableGenerator.SEPARATOR + j // the name of the table
+                        + " (\"" + i + "\" int, \"" + j + "\" int)" // the columns of the table
+                        + ";" + TableGenerator.NEWLINE //teh ending of the query
+                        ;
+
+                // Second insert values into the table
+                String itp = "INSERT INTO " // insert statement/command
+                        + "edge" + i + "x" + j // the name of the table
+                        + " VALUES " // the value command
+                        ;
+
+                String it = "";
+                // For each combination, insert
+                for (Pair p : TableGenerator.COLOURCOMBINATIONS) {
+                    it += itp + p.toString() // add the actual values to the query
+                            + ";" + TableGenerator.NEWLINE; // add ending of the query
+                }
+
+                // adding the SQL statements for this edge to the overall statement
+                statement += ct + it + TableGenerator.NEWLINE;;
+            }
+        }
+
+        return statement;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String out = TableGenerator.GenerateTable(4);
+
+        System.out.println(out);
+    }
+}
