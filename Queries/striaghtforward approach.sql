bcb7a42853b5779414e21e22f6ee6da7259913b3
Colin-TUE <c.lambrechts@student.tue.nl>
2014-06-09 13:01:53 +0200

    added final report files

---
+++ Queries/striaghtforward approach.sql
@@ -0,0 +1,13 @@
+﻿SELECT edge6x13."6"
+FROM edge6x13
+JOIN edge1x15 ON (1=1)
+JOIN edge2x19 ON (1=1)
+JOIN edge5x11 ON (1=1)
+JOIN edge7x15 ON (1=1)
+JOIN edge2x9 ON (1=1)
+JOIN edge11x15 ON (1=1)
+JOIN edge0x19 ON (1=1)
+JOIN edge8x15 ON (1=1)
+JOIN edge4x11 ON (1=1)
+
+WHERE (1=1)
---
+++ Queries/test3.sql
@@ -0,0 +1,8 @@
+﻿SELECT *
+FROM edge0x1
+JOIN edge1x2 ON (1=1) AND edge0x1."1" = edge1x2."1"
+JOIN edge0x2 ON edge0x1."0" = edge0x2."0" AND edge1x2."2" = edge0x2."2"
+JOIN edge0x3 ON edge0x1."0" = edge0x3."0"
+JOIN edge1x3 ON edge0x1."1" = edge1x3."1" AND edge0x3."3" = edge1x3."3"
+JOIN edge2x3 ON edge1x2."2" = edge2x3."2" AND edge0x2."2" = edge2x3."2" 
+   AND edge0x3."3" = edge1x3."3" AND edge1x3."3" = edge2x3."3"
